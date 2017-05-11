Elphel/elphel-apps-camogm

commit 076161cdd30ed6d349a4f53e2c8bca8d3745fbd3

Check compressors' state and get disk parameters before recording starts

src/camogm.c
@@ -166,6 +166,7 @@ static uint64_t get_disk_size(const char *name);
 static int get_sysfs_name(const char *dev_name, char *sys_name, size_t str_sz, int type);
 static int get_disk_range(const char *name, struct range *rng);
 static int set_disk_range(const struct range *rng);
+static void get_disk_info(camogm_state *state);
 int open_files(camogm_state *state);
 unsigned long getGPValue(unsigned int port, unsigned long GPNumber);
 void setGValue(unsigned int port, unsigned long GNumber, unsigned long value);
@@ -841,30 +842,39 @@ void  camogm_set_ignore_fps(camogm_state *state, int d)
  */
 void  camogm_set_prefix(camogm_state *state, const char * p, path_type type)
 {
-	struct range rng = {0};
-
 	if (type == FILE_PATH) {
 		strncpy(state->path_prefix, p, sizeof(state->path_prefix) - 1);
 		state->path_prefix[sizeof(state->path_prefix) - 1] = '\0';
 	} else if (type == RAW_PATH && (strncmp(p, "/dev/", 5) == 0)) {
 		strncpy(state->rawdev.rawdev_path, p, sizeof(state->rawdev.rawdev_path) - 1);
 		state->rawdev.rawdev_path[sizeof(state->rawdev.rawdev_path) - 1] = '\0';
-		state->rawdev.end_pos = get_disk_size(state->rawdev.rawdev_path);
-		if (state->rawdev.end_pos == 0) {
-			state->rawdev_op = 0;
-			state->rawdev.end_pos = state->rawdev.start_pos;
-			state->rawdev.rawdev_path[0] = '\0';
-			D0(fprintf(debug_file, "ERROR: unable to initiate raw device operation\n"));
-		} else {
-			D0(fprintf(debug_file, "WARNING: raw device write initiated\n"));
-			state->rawdev_op = 1;
-		}
+	}
+}
 
-		if (get_disk_range(state->rawdev.rawdev_path, &rng) == 0) {
-			set_disk_range(&rng);
-		} else {
-			D0(fprintf(debug_file, "ERROR: unable to get disk size and starting sector\n"));
-		}
+/**
+ * @brief Get disk size, first LBA, last LBA and save these parameters in state structure.
+ * @param[in]   state   a pointer to a structure containing current state
+ * @return      None
+ */
+void get_disk_info(camogm_state *state)
+{
+	struct range rng = {0};
+
+	state->rawdev.end_pos = get_disk_size(state->rawdev.rawdev_path);
+	if (state->rawdev.end_pos == 0) {
+		state->rawdev_op = 0;
+		state->rawdev.end_pos = state->rawdev.start_pos;
+		state->rawdev.rawdev_path[0] = '\0';
+		D0(fprintf(debug_file, "ERROR: unable to initiate raw device operation\n"));
+	} else {
+		D0(fprintf(debug_file, "WARNING: raw device write initiated\n"));
+		state->rawdev_op = 1;
+	}
+
+	if (get_disk_range(state->rawdev.rawdev_path, &rng) == 0) {
+		set_disk_range(&rng);
+	} else {
+		D0(fprintf(debug_file, "ERROR: unable to get disk size and starting sector\n"));
 	}
 }
 
@@ -1263,6 +1273,8 @@ int parse_cmd(camogm_state *state, FILE* npipe)
 	}
 // now cmd is trimmed, arg is NULL or a pointer to trimmed command arguments
 	if (strcmp(cmd, "start") == 0) {
+		check_compressors(state);
+		get_disk_info(state);
 		camogm_start(state);
 		return 1;
 	} else if (strcmp(cmd, "reset") == 0) { // will reset pointer to the last acquired frame (if any)

commit df1c1959282df9f5dc20dbcfc5632d20f470178f

Do not reset disabled channels after start
This commit fixes a bug introduced in 076161cdd30 where a list of active
sensor ports is updated every time recording has started.

src/camogm.c
@@ -223,6 +223,8 @@ void check_compressors(camogm_state *state)
 		if (getGPValue(i, P_COMPRESSOR_RUN) != COMPRESSOR_RUN_STOP) {
 			state->active_chn |= 1 << i;
 		}
+		// restore the state of disabled channels
+		state->active_chn &= state->active_chn_mask;
 	}
 }
 
@@ -274,6 +276,7 @@ void camogm_init(camogm_state *state, char *pipe_name, uint16_t port_num)
 	state->rawdev.curr_pos_w = state->rawdev.start_pos;
 	state->rawdev.curr_pos_r = state->rawdev.start_pos;
 	state->active_chn = ALL_CHN_INACTIVE;
+	state->active_chn_mask = ALL_CHN_ACTIVE;
 	state->rawdev.mmap_default_size = MMAP_CHUNK_SIZE;
 	state->sock_port = port_num;
 
@@ -2077,10 +2080,13 @@ inline int is_chn_active(camogm_state *s, unsigned int port)
 inline void set_chn_state(camogm_state *s, unsigned int port, unsigned int new_state)
 {
 	if (port >= 0 && port < SENSOR_PORTS) {
-		if (new_state)
+		if (new_state) {
 			s->active_chn |= 1 << port;
-		else
+			s->active_chn_mask |= 1 << port;
+		} else {
 			s->active_chn &= ~(1 << port);
+			s->active_chn_mask &= ~(1 << port);
+		}
 	}
 }
 

src/camogm.h
@@ -256,6 +256,8 @@ typedef struct {
 	int rawdev_op;                                          ///< flag indicating writing to raw device
 	rawdev_buffer rawdev;                                   ///< contains pointers to raw device buffer
 	unsigned int active_chn;                                ///< bitmask of active sensor ports
+	unsigned int active_chn_mask;                           ///< bitmask of enabled sensor ports; used to save enabled/disabled ports between
+	                                                        ///< consecutive starts/stops as active ports list is updated on each start
 	uint16_t sock_port; 									///< command socket port number
 	struct writer_params writer_params;                     ///< contains control parameters for writing thread
 	unsigned int error_stat[SENSOR_PORTS][CAMOGM_ERRNUM];   ///< collect statistics about errors